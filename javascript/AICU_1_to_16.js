2621bc381c4b7876ba19a09b0d7cbb036b0fffe0
Rory Yeung <Rory.Yeung@Googlemail.com>
2021-07-01 00:20:01 +0100

    Javascript test

--- javascript/AICU_1_to_16.js
+++ javascript/AICU_1_to_16.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-let announcement = 'Some long string'
-
-function announcements() {
-    document.getElementById("announcements text").innerText=announcement
-};
-
-announcements();
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", "/Data/Announcement.txt", false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                 document.querySelector("announcements-text").value = allText;
+            }
+        }
+    }
+    rawFile.send(null);
+}
+setInterval(readTextFile, 1000);
