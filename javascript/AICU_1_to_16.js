1ec89e4c29d0e7baaf8d248574582213e7220b9e
Rory Yeung <Rory.Yeung@Googlemail.com>
2021-07-01 00:01:12 +0100

    Added react

---
+++ javascript/AICU_1_to_16.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+let announcement = 'Some long string'
+
+function announcements() {
+    document.getElementById("announcements text").innerText=announcement
+};
+
+announcements();
